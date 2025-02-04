45d59c2c889706b53ed62b22ed5cda49135abd40
Abraham-MJ <imnoteverywhere12@gmail.com>
2025-02-04 01:11:22 -0400

    feat: Configure prettier sorting

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-"use client";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return <></>;
+export default async function Home() {
+  redirect("/main/dashboard");
 }
