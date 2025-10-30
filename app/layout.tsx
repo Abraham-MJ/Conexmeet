fe28cb1264ee184666a3ba00eb3031a9dbcfd1af
Abraham-maker <morenoabraham.j@gmail.com>
2025-10-30 14:51:58 -0400

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import SessionProviderWrapper from './components/SessionProviderWrapper';
 import { LanguageProvider } from './context/useLanguageContext';
@@ -22,7 +23,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <head></head>
       <body className={`${poppins.variable} antialiased`}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YDKVM39PVG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YDKVM39PVG');
+          `}
+        </Script>
+
         <LanguageProvider>
           <SessionProviderWrapper>
             {children}
